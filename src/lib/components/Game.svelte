94f705919cd089ccaece73f6710e2897b3e6e1b6
Kresna Satya <kresnasatya72@gmail.com>
2024-10-28 12:11:49 +0800

    feat: finish exercise 1

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,10 +1,11 @@
 <script>
     import { sample } from "../utils";
     import { WORDS } from "../data";
+    import GameInput from "./GameInput.svelte";
 
     // Pick a random word on every pageload.
     const answer = sample(WORDS);
     console.info({ answer });
 </script>
 
-<p>Put a game here!</p>
+<GameInput/>
---
+++ src/lib/components/GameInput.svelte
@@ -0,0 +1,15 @@
+<script>
+    let guess = $state('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(guess);
+    }
+</script>
+
+<form onsubmit={handleSubmit} class="guess-input-wrapper">
+    <label for="guess-input">Enter guess:</label>
+    <input required type="text" id="guess-input" minlength="5" maxlength="5" pattern={`[A-Z]{5}`} title="5 letter word" bind:value={guess} oninput={(event) => {
+        guess = event.target.value.toUpperCase();
+    }}>
+</form>
